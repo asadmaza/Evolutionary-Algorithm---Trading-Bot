c48f995fd8e0c9d91e91a9069707fe11997833b0
SodaVolcano <SodaVolcano@users.noreply.github.com>
2023-05-02 09:51:01 +0000

    Automated changes

--- daily_optimal.py
+++ daily_optimal.py
@@ -8,39 +8,39 @@ from fitness import Fitness
 
 
 class Optimal(Strategy):
-    def __init__(self, candles) -> None:
-        super().__init__(candles)
+  def __init__(self, candles) -> None:
+    super().__init__(candles)
 
-        # Duplicate the first value - fixes the looping
-        self.close.loc[-1] = self.close[0]
-        self.close.index = self.close.index + 1
-        self.close = self.close.sort_index()
+    # Duplicate the first value - fixes the looping
+    self.close.loc[-1] = self.close[0]
+    self.close.index = self.close.index + 1
+    self.close = self.close.sort_index()
 
-        # Removing indicators from graph
-        self.n_indicators = 0
+    # Removing indicators from graph
+    self.n_indicators = 0
 
-    def buy_trigger(self, t: int) -> bool:
-        if t + 1 >= len(self.close):
-            return False
-        return self.close[t + 1] > self.close[t]
+  def buy_trigger(self, t: int) -> bool:
+    if t + 1 >= len(self.close):
+      return False
+    return self.close[t + 1] > self.close[t]
 
-    def sell_trigger(self, t: int) -> bool:
-        if t + 1 >= len(self.close):
-            return False
-        return self.close[t + 1] < self.close[t]
+  def sell_trigger(self, t: int) -> bool:
+    if t + 1 >= len(self.close):
+      return False
+    return self.close[t + 1] < self.close[t]
 
 
 if __name__ == "__main__":
-    """
-    Testing
-    """
+  """
+  Testing
+  """
 
-    from candle import get_candles
+  from candle import get_candles
 
-    train_candles, test_candles = get_candles_split(0.2)
+  train_candles, test_candles = get_candles_split(0.2)
 
-    o = Optimal(train_candles)
-    portfolio = o.evaluate(False)
-    f = Fitness(batches=4)
-    o.update_fitness(f.get_fitness(o))
-    print(portfolio)
+  o = Optimal(train_candles)
+  portfolio = o.evaluate(False)
+  f = Fitness(batches=4)
+  o.update_fitness(f.get_fitness(o))
+  print(portfolio)
