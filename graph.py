39bbee860436f4d1f41170670deb02a67927471b
Jasper Paterson <jasperpato@gmail.com>
2023-05-10 13:07:42 +0800

    finalised-results-and-graphs

--- graph.py
+++ graph.py
@@ -4,9 +4,12 @@ from random_strategy import RandomStrategy
 from simple_strategy import SimpleStrategy
 import pickle
 from strategy import Strategy
+from fitness import Fitness
 
-BEST = 'results/sortino100/best_strategies_tournament{}.pkl'
+BEST_PORTFOLIO = 'results/portfolio100/best_strategies_tournament{}.pkl'
+BEST_SORTINO = 'results/sortino200/best_strategies_tournament{}.pkl'
 
+fit = Fitness()
 
 if __name__ == '__main__':
   _, candles = get_candles_split()
@@ -14,14 +17,17 @@ if __name__ == '__main__':
   r = RandomStrategy(candles)
   r.evaluate(graph=True, fname='graphs/random.png',
              title='Random strategy, test data')
+  print(f'random {r.portfolio:.4f} {fit.get_fitness(r):.4f}')
 
   s = SimpleStrategy(candles)
   s.evaluate(graph=True, fname='graphs/simple.png',
              title='Simple strategy, test data')
+  print(f'simple {s.portfolio:.4f} {fit.get_fitness(s):.4f}')
 
+  # portfolio
   best = None
   for i in range(10):
-    with open(BEST.format(i), "rb") as f:
+    with open(BEST_PORTFOLIO.format(i), "rb") as f:
       data = pickle.load(f)
     strats = [Strategy.load_pickle_data(candles, d) for d in data]
     local_best = sorted(strats, key=lambda s: s.portfolio)[-1]
@@ -30,5 +36,24 @@ if __name__ == '__main__':
 
   best.evaluate(
       graph=True,
-      fname='graphs/best.png',
-      title='Best strategy, test data')
+      fname='graphs/best_portfolio.png',
+      title='Best strategy portfolio')
+  
+  print(f'{best.portfolio:.4f}')
+  
+  # sortino
+  best = None
+  for i in range(10):
+    with open(BEST_SORTINO.format(i), "rb") as f:
+      data = pickle.load(f)
+    strats = [Strategy.load_pickle_data(candles, d) for d in data]
+    local_best = sorted(strats, key=lambda s: fit.get_fitness(s))[-1]
+    if not best or fit.get_fitness(local_best) > fit.get_fitness(best):
+      best = local_best
+
+  best.evaluate(
+      graph=True,
+      fname='graphs/best_sortino.png',
+      title=f'Best strategy sortino = {fit.get_fitness(best):.4f}')
+
+  print(f'{fit.get_fitness(best):.4f}')
